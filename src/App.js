c710136f2cb8e972d8f1290f1df3a76eb49474fa
ma044ka <mayank.kasturia@pb.com>
2020-01-03 15:50:22 -0700

    update app.js for feature branch 2

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Pitney Bowes Geocoding
+          learn react
         </a>
       </header>
     </div>
